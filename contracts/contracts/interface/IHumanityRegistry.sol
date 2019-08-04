e94f0c53b109d2806d07bba233f2a45ca584debb
nate welch <nathanielawelch@gmail.com>
2019-08-04 18:28:04 -0400

    Committed github pages.

--- contracts/contracts/interface/IHumanityRegistry.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.7;
-pragma experimental ABIEncoderV2;
-
-
-contract IHumanityRegistry {
-    function isHuman(address who) public view returns (bool);
-}
---
+++ precache-manifest.8b08338f96dfb99a5672b344038d745f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d37c0cc98d71160a01851804301678de",
+    "url": "/humanity-compound-ubi-site/static/media/humanityIcon.d37c0cc9.svg"
+  },
+  {
+    "revision": "cfb65a48aebbae19a7d184b06b3900ee",
+    "url": "/humanity-compound-ubi-site/static/media/daiIcon.cfb65a48.svg"
+  },
+  {
+    "revision": "a4e875b5428b2856e15b",
+    "url": "/humanity-compound-ubi-site/static/js/runtime~main.a4e875b5.js"
+  },
+  {
+    "revision": "1618d1ad54bc0d53aed1",
+    "url": "/humanity-compound-ubi-site/static/js/main.1618d1ad.chunk.js"
+  },
+  {
+    "revision": "e286928f9ae2ab8e54cb",
+    "url": "/humanity-compound-ubi-site/static/js/1.e286928f.chunk.js"
+  },
+  {
+    "revision": "1618d1ad54bc0d53aed1",
+    "url": "/humanity-compound-ubi-site/static/css/main.3fe1fd86.chunk.css"
+  },
+  {
+    "revision": "e286928f9ae2ab8e54cb",
+    "url": "/humanity-compound-ubi-site/static/css/1.b9e38518.chunk.css"
+  },
+  {
+    "revision": "3a0ec6c9a3232ed0c2afc2284405ac1a",
+    "url": "/humanity-compound-ubi-site/index.html"
+  }
+];
